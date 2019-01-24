5d93597f50caa81c8156a86f9cd9ac3c1bfe5d19
Justin <4533867+Justin991q@users.noreply.github.com>
2019-01-25 00:39:27 +0100

    getLastCommitId returned null on Windows

--- src/GitRepository.php
+++ src/GitRepository.php
@@ -377,14 +377,14 @@
 
 		/**
 		 * Returns last commit ID on current branch
-		 * `git log --pretty=format:'%H' -n 1`
+		 * `git log --pretty=format:"%H" -n 1`
 		 * @return string|NULL
 		 * @throws GitException
 		 */
 		public function getLastCommitId()
 		{
 			$this->begin();
-			$lastLine = exec('git log --pretty=format:\'%H\' -n 1 2>&1');
+			$lastLine = exec('git log --pretty=format:"%H" -n 1 2>&1');
 			$this->end();
 			if (preg_match('/^[0-9a-f]{40}$/i', $lastLine)) {
 				return $lastLine;
