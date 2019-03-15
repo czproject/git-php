452b81c4a9468667163f35a0a63f343fec498804
Ulrich Schmidt <admin@deruli.de>
2019-03-15 16:33:29 +0100

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/src/IGit.php';
-require __DIR__ . '/src/GitRepository.php';
-
-use Cz\Git\GitRepository;
-
-$repo = new GitRepository(".");
-var_dump($repo->getChanges());
