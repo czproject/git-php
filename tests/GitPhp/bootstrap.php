807e59f077b5a3d555467ff4d9a44fb9a309adff
Jan Pecha <janpecha@email.cz>
2022-04-24 14:20:53 +0200

    Tests: test() catches GitException and dumps output & exit code

--- tests/GitPhp/bootstrap.php
+++ tests/GitPhp/bootstrap.php
@@ -8,5 +8,24 @@ Tester\Environment::setup();
 
 function test($cb)
 {
-	$cb();
+	try {
+		$cb();
+
+	} catch (CzProject\GitPhp\GitException $e) {
+		$result = $e->getRunnerResult();
+
+		if ($result !== NULL) {
+			echo $result->getCommand(), "\n";
+			echo 'EXIT CODE: ', $result->getExitCode(), "\n";
+			echo "--------------\n",
+				$result->getOutputAsString(), "\n";
+
+			if ($result->hasErrorOutput()) {
+				echo "--------------\n",
+					implode("\n", $result->getErrorOutput()), "\n";
+			}
+		}
+
+		throw $e;
+	}
 }
